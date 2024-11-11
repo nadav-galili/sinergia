c71e9e21afb9273f50c76f1f49c6deb52e27c9d1
nadav-mobileBrain <nadavg@mobile-brain.net>
2024-11-11 09:23:35 +0200

    unused vars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Categories from "@/components/Categories";
 import Parallex from "@/components/Parallex";
-import Academy from "@/components/Academy";
-import RetailAcademy from "@/components/RetailAcademy";
+// import Academy from "@/components/Academy";
+// import RetailAcademy from "@/components/RetailAcademy";
 
 export default function Home() {
   return (
