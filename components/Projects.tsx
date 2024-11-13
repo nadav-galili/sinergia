c627eb7bab35bac513082fc8df3e0712a592764c
nadav-mobileBrain <nadavg@mobile-brain.net>
2024-11-13 15:43:30 +0200

    changed fonts and added new partners img

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -45,17 +45,19 @@ const successStories: SuccessStory[] = [
 
 const Projects = () => {
   return (
-    <section className="py-16 px-4  mx-auto">
-      <h2 className="heading rounded-lg mx-auto">פרוייקטים לדוגמה</h2>
-      <h3 className="text-30-semibold !text-primary underline text-center mb-5 ">
-        ההצלחות שלנו
-      </h3>
+    <section className="container mx-auto mt-24 px-4 md:px-16">
+      <div className="bg-primary pattern flex justify-center items-center flex-col py-10 mb-10 px-6 rounded-lg ">
+        <h2 className="heading  rounded-lg mx-auto">פרוייקטים לדוגמה</h2>
+        <h3 className="text-20-medium !text-primary bg-white px-6 py-3 rounded-lg underline text-center mb-5 ">
+          ההצלחות שלנו
+        </h3>
+      </div>
 
       <div className="grid md:grid-cols-3 gap-5">
         {successStories.map((story, index) => (
           <div
             key={index}
-            className="bg-white rounded-lg shadow-lg p-8 hover:shadow-xl transition-shadow"
+            className="bg-white text-center rounded-lg shadow-lg p-8 hover:shadow-xl transition-shadow"
             tabIndex={0}
             role="article"
             aria-label={`Success story: ${story.title}`}>
@@ -66,11 +68,10 @@ const Projects = () => {
             <p className="text-black-100 mb-6">{story.description}</p>
 
             {/* <h4 className="font-semibold mb-3">Key Achievements:</h4> */}
-            <ul className="space-y-2">
+            <ul className="space-y-2 ">
               {story.achievements.map((achievement, i) => (
-                <li key={i} className="flex items-start">
+                <li key={i} className="flex text-center items-center ">
                   <Trophy className="w-5 h-5 mx-2 text-success" />
-                  {/* <span className="text-green-500 mr-2">•</span> */}
                   {achievement}
                 </li>
               ))}
